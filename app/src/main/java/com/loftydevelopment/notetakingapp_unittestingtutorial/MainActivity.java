43f3b1615f3b4492e7465dac002d629885cff07b
Connor Loftus <connorlof@gmail.com>
2019-10-14 13:16:47 -0400

    Initial commit

---
+++ app/src/main/java/com/loftydevelopment/notetakingapp_unittestingtutorial/MainActivity.java
@@ -0,0 +1,14 @@
+package com.loftydevelopment.notetakingapp_unittestingtutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
